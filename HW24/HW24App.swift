d3c71a02bd63827e4d89d19a32d528116441fb1c
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2022-05-21 12:18:48 +0300

    Initial Commit

---
+++ HW24/HW24App.swift
@@ -0,0 +1,17 @@
+//
+//  HW24App.swift
+//  HW24
+//
+//  Created by Александр Петрович on 21.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
